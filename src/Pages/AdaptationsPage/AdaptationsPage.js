4cc4c89f30d17da923dba74aafb55c1120f18b89
Ilyas Shahmuradov <ilyas.shahmuradov@mail.ru>
2024-09-01 05:45:55 +0400

    Search developed

--- src/Pages/AdaptationsPage/AdaptationsPage.js
+++ src/Pages/AdaptationsPage/AdaptationsPage.js
@@ -9,16 +9,50 @@ import { AdaptationFiltration } from "../../Components/AdaptationFiltration/Adap
 export const AdaptationsPage = () => {
   const title = "Каталог Адаптаций";
 
-  const { adaptationsArr } = useContext(MainPageContext);
+  const { adaptationsArr, setAdaptationsArr } = useContext(MainPageContext);
+
+  const searchAdaptation = (input) => {
+    if (input !== "") {
+      const newArr = adaptationsArr.filter((adaptation) => {
+        if (
+          adaptation.name.toLowerCase().includes(input.toLowerCase()) ||
+          adaptation.original_name.toLowerCase().includes(input.toLowerCase())
+        ) {
+          return true;
+        }
+      });
+
+      setAdaptationsArr(newArr);
+    } else {
+      fetch("https://66ce31f6199b1d6286880fb0.mockapi.io/games/adaptations")
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          setAdaptationsArr(data);
+        });
+    }
+  };
   return (
     <div className="adaptations-page-wrapper">
       <Banner title={title} img={adaptationBanner} />
       <div className="adaptations-page-container">
-        <div className="adaptations-page-container-filtration">
-          <div className="adaptations-page-container-filtration-title">
-            Фильтрация
+        <div className="adaptations-page-container-search-filtration">
+          <div className="adaptations-page-container-search">
+            <div className="adaptations-page-container-search-title">Поиск</div>
+            <input
+              type="text"
+              onChange={(e) => {
+                searchAdaptation(e.target.value);
+              }}
+            />
           </div>
-          <AdaptationFiltration />
+          <div className="adaptations-page-container-filtration">
+            <div className="adaptations-page-container-filtration-title">
+              Фильтрация
+            </div>
+            <AdaptationFiltration />
+          </div>
         </div>
         <div className="adaptations-page-container-items">
           <AdaptationsItems adaptationsArr={adaptationsArr} nthChild={3} />
--- src/Pages/GamesPage/GamesPage.js
+++ src/Pages/GamesPage/GamesPage.js
@@ -9,17 +9,49 @@ import { GamesFiltration } from "../../Components/GamesFiltration/GamesFiltratio
 export const GamesPage = () => {
   const title = "Каталог Игр";
 
-  const { gamesArr } = useContext(MainPageContext);
+  const { gamesArr, setGamesArr } = useContext(MainPageContext);
+
+  const searchGames = (input) => {
+    if (input !== "") {
+      const newArr = gamesArr.filter((game) => {
+        if (game.name.toLowerCase().includes(input.toLowerCase())) {
+          return true;
+        }
+      });
+
+      setGamesArr(newArr);
+    } else {
+      fetch("https://66ce31f6199b1d6286880fb0.mockapi.io/games/games")
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          setGamesArr(data);
+        });
+    }
+  };
   return (
     <div className="games-page-wrapper">
       <Banner title={title} img={gameBanner} />
       <div className="games-page-container">
-        <div className="games-page-container-filtration">
-          <div className="games-page-container-filtration-title">
-            Фильтрация
+        <div className="games-page-container-search-filtration">
+          <div className="games-page-container-search">
+            <div className="games-page-container-search-title">Поиск</div>
+            <input
+              type="text"
+              onChange={(e) => {
+                searchGames(e.target.value);
+              }}
+            />
           </div>
-          <GamesFiltration />
+          <div className="games-page-container-filtration">
+            <div className="games-page-container-filtration-title">
+              Фильтрация
+            </div>
+            <GamesFiltration />
+          </div>
         </div>
+
         <div className="games-page-container-items">
           <GamesItems gamesArr={gamesArr} nthChild={2} />
         </div>
